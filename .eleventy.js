4a413520f625d5f927a73f0824f5380eab6e7b29
jones58 <108466184+jones58@users.noreply.github.com>
2023-11-29 12:07:14 +0000

    edit config to make appear as root directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,5 @@
 module.exports = function (eleventyConfig) {
-  return {
-    dir: {
-      input: "src",
-      output: "public",
-    },
-  };
-
   eleventyConfig.setBrowserSyncConfig({
-    // Other configuration options...
     serveStatic: [
       {
         route: "/index.html",
@@ -15,4 +7,10 @@ module.exports = function (eleventyConfig) {
       },
     ],
   });
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
 };
